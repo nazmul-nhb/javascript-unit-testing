9d557e449a399976edfd1069d4aa0ede48259430
Nazmul Hassan <nazmulnhb007@yahoo.com>
2025-07-28 13:01:44 +0600

    update(tests): added some demo/primary test to know some other assertion

---
+++ __tests__/test.test.js
@@ -0,0 +1,41 @@
+// @ts-check
+
+import { describe, expect, it } from "vitest";
+
+describe('test non-primitives', () => {
+    const res = { name: "NHB" };
+
+    it('test object', () => {
+        expect(res).toEqual({ name: "NHB" });
+    })
+
+    it('test object matcher', () => {
+        expect(res).toMatchObject({ name: "NHB" });
+    })
+
+    it('test object property matcher', () => {
+        expect(res).toHaveProperty('name');
+    })
+});
+
+describe('good assertions', () => {
+    const res = "Hello there...";
+
+    it('too general', () => {
+        // Loose (too general) 
+        // Not Valuable
+        expect(res).toBeDefined();
+    });
+
+    it('too specific', () => {
+        // Tight (too specific) 
+        // Not Valuable
+        expect(res).toBe("Hello there...");
+    })
+
+    it('better assertion', () => {
+        // Better assertion 
+        expect(res).toMatch(/[aA-zZ]/);
+    })
+});
+
